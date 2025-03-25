e471e6258d744428513e66fafa79d31f5b72eaa0
0xkowalskidev <0xkowalskidev@gmail.com>
2025-03-25 02:32:02 +0000

    Added build optimizations for size

--- flake.nix
+++ flake.nix
@@ -8,8 +8,12 @@
       name = "nopswd";
       src = self;
       buildInputs = [ nixpkgs.legacyPackages.x86_64-linux.odin ];
-      buildPhase = "odin build . -out:nopswd";
-      installPhase = "mkdir -p $out/bin; cp nopswd $out/bin/";
+      buildPhase = "odin build . -out:nopswd -o:size -no-bounds-check";
+      installPhase = ''
+        strip nopswd
+        mkdir -p $out/bin
+        cp nopswd $out/bin/
+      '';
     };
   };
 }
